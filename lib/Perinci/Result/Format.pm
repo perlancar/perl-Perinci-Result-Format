07791fb399444f87827dc51bc6a7ae747c0bd7ea
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-06-18 11:46:36 +0700

    Clone and clean instead of clean in-place to avoid modifying e.g. Rinci metadata cache in a daemon

--- lib/Perinci/Result/Format.pm
+++ lib/Perinci/Result/Format.pm
@@ -158,7 +158,7 @@ sub format {
             require Data::Clean::JSON;
             $cleanser = Data::Clean::JSON->get_cleanser;
         }
-        $cleanser->clean_in_place($res);
+        $cleanser->clone_and_clean($res);
     }
 
     my $deco = $Enable_Decoration;
